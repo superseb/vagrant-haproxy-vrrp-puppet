fea81f1944a021372b24eac54f723ad02c72da0e
superseb <seb@nospam.fake>
2014-08-06 17:17:29 +0200

    Include haproxy and proper keepalived

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,5 @@
 node /haproxy01/ {
+  include ::haproxy
   include ::keepalived
 
   keepalived::vrrp::script { 'check_haproxy':
@@ -12,18 +13,50 @@ node /haproxy01/ {
     priority          => '101',
     auth_type         => 'PASS',
     auth_pass         => 'secret',
-    virtual_ipaddress => '10.0.0.1/29',
+    virtual_ipaddress => '172.28.33.12',
     track_script      => 'check_haproxy',
+  }
+
+  haproxy::listen { 'web':
+    collect_exported => false,
+    ipaddress        => $::ipaddress,
+    ports            => '80',
+    options          => {},
+  }
+  haproxy::listen { 'stats':
+    ipaddress => $::ipaddress, 
+    ports     => '8080',
+    options   => {
+      'mode'  => 'http',
+      'stats' => [
+        'uri /',
+        'auth puppet:puppet'
+      ],  
+    },  
+  }
+  haproxy::balancermember { 'web01':
+    listening_service => 'web',
+    server_names      => 'web01',
+    ipaddresses       => '172.28.33.21',
+    ports             => '80',
+    options           => 'check',
+  }
+  haproxy::balancermember { 'web02':
+    listening_service => 'web',
+    server_names      => 'web02',
+    ipaddresses       => '172.28.33.22',
+    ports             => '80',
+    options           => 'check',
   }
 }
 
 node /haproxy02/ {
+  include ::haproxy
   include ::keepalived
 
   keepalived::vrrp::script { 'check_haproxy':
     script => '/usr/bin/killall -0 haproxy',
   }
-
   keepalived::vrrp::instance { 'VI_50':
     interface         => 'eth1',
     state             => 'BACKUP',
@@ -31,7 +64,42 @@ node /haproxy02/ {
     priority          => '100',
     auth_type         => 'PASS',
     auth_pass         => 'secret',
-    virtual_ipaddress => '10.0.0.1/29',
+    virtual_ipaddress => '172.28.33.12',
     track_script      => 'check_haproxy',
   }
+  haproxy::listen { 'stats':
+    ipaddress => $::ipaddress, 
+    ports     => '8080',
+    options   => {
+      'mode'  => 'http',
+      'stats' => [
+        'uri /',
+        'auth puppet:puppet'
+      ],  
+    },  
+  }
+  haproxy::listen { 'web':
+    collect_exported => false,
+    ipaddress        => $::ipaddress,
+    ports            => '80',
+    options          => {},
+  }
+  haproxy::balancermember { 'web01':
+    listening_service => 'web',
+    server_names      => 'web01',
+    ipaddresses       => '172.28.33.21',
+    ports             => '80',
+    options           => 'check',
+  }
+  haproxy::balancermember { 'web02':
+    listening_service => 'web',
+    server_names      => 'web02',
+    ipaddresses       => '172.28.33.22',
+    ports             => '80',
+    options           => 'check',
+  }
+
+}
+
+node default {
 }
